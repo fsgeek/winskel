75ebdd56b5993453800c66e0e341fcc3a695e3ef
Tony Mason <fsgeek@gatech.edu>
2019-05-04 13:31:32 -0700

    Turn on code analysis, don't track *.vcxproj.user files since those are local settings

--- winskel/winskel.cpp
+++ winskel/winskel.cpp
@@ -2,10 +2,12 @@
 #include <ntstatus.h>
 
 extern "C" {
-	NTSTATUS DriverEntry(PDRIVER_OBJECT DriverObject, PUNICODE_STRING RegistryPath);
+	NTSTATUS DriverEntry(PDRIVER_OBJECT DriverObject, const PUNICODE_STRING RegistryPath);
 }
 
-NTSTATUS DriverEntry(PDRIVER_OBJECT DriverObject, PUNICODE_STRING RegistryPath)
+DRIVER_INITIALIZE DriverEntry;
+#pragma ignore(warning:26461) // we don't mark it as const because we will be modifying it
+NTSTATUS DriverEntry(PDRIVER_OBJECT DriverObject, const PUNICODE_STRING RegistryPath)
 {
 	UNREFERENCED_PARAMETER(DriverObject);
 	UNREFERENCED_PARAMETER(RegistryPath);
